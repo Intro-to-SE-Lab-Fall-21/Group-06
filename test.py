4d14866bef0ae5756126bd898739bbf8fcf0ae7e
KaranSinghmods <89550353+KaranSinghmods@users.noreply.github.com>
2021-11-19 22:50:18 -0600

    1. Included some missing libraries in the file 2. Corrected a missing ' in def test_home_page() 3. Separated test_travis() into multiple tests

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ def test_home_page():
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
-                                   
-def test_travis():
-    in_server = 'smtp.gmail.com'
-    in_port = 465
-    context = ssl.create_default_context()
 
+in_server = 'smtp.gmail.com'
+in_port = 465
+context = ssl.create_default_context()
+
+def test_travis1():
     print("Test 1: Login with wrong credentials.")
     email = "sth@gmail.com"
     password = "somepassword"
@@ -26,6 +26,7 @@ def test_travis():
         except:
             print("PASS")
 
+def test_travis2():
     print("Test 2: Login with wrong password/correct email.")
     userEmail = "introsetest1@gmail.com"
     userPassword = "somePassword"
@@ -36,6 +37,7 @@ def test_travis():
         except:
             print("PASS")
 
+def test_travis3():
     print("Test 3: Login with correct credentials.")  # setting up email
     userEmail = "introsetest1@gmail.com"
     userPassword = "!ntr0test"
@@ -47,6 +49,7 @@ def test_travis():
         except:
             print("FAIL")
 
+def test_travis4():
     print("Test 4: Create email with no sender, but includes a subject and body.")
     userEmail = "introsetest1@gmail.com"
     userPassword = "!ntr0test"
@@ -63,6 +66,7 @@ def test_travis():
         except:
             print("PASS")
 
+def test_travis5():
     print("Test 5: Create email")
     userEmail = "introsetest1@gmail.com"
     userPassword = "!ntr0test"
@@ -79,6 +83,7 @@ def test_travis():
         except:
             print("FAIL")
 
+def test_travis6():
     print("Test 6: Navigating to login page")
     try:
         with app.test_client() as test_client:
@@ -88,6 +93,7 @@ def test_travis():
     except:
         print("FAIL")
 
+def test_travis7():
     print("Test 7: Navigating to inbox with unauthorized access")
     try:
         with app.test_client() as test_client:
@@ -97,6 +103,7 @@ def test_travis():
     except:
         print("FAIL")
 
+def test_travis8():
     print("Test 8: Navigating to inbox with authorized access")
     email = "introsetest1@gmail.com"
     password = "!ntr0test"
@@ -105,4 +112,4 @@ def test_travis():
             server.login(email, password)
             print("Test passed for correct credentials.")
         except:
-            print("Test failed for correct credentials.")                              
+            print("Test failed for correct credentials.")
