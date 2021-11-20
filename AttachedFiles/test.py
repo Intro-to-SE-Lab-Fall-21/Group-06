720aec48a7e8e7f9ee6f147be3e1a130a2f0d897
KaranSinghmods <89550353+KaranSinghmods@users.noreply.github.com>
2021-11-19 21:59:43 -0600

    Delete test.py

--- AttachedFiles/test.py
+++
@@ -1,8 +0,0 @@
-from website import create_app
-
-flask_app = create_app()
-
-def test_home_page():
-    with flask_app.test_client() as test_client:
-        response = test_client.get('/)
-        assert response.status_code == 200
